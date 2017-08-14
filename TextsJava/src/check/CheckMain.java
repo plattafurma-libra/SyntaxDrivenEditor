08e3b30812f495fcb8bde49838dfbb560a056c0d
Rolshoven <rols@spinfo.uni-koeln.de>
2017-08-14 13:06:29 +0200

    aser

---
+++ TextsJava/src/check/CheckMain.java
@@ -0,0 +1,14 @@
+package check;
+
+import CP.TextsText.*;
+
+
+/*generates data structure TextsText_Text which might be transformed to json */
+
+public class CheckMain {
+	public static void main(String[] args) {
+				
+	System.out.println("CheckMain");
+	TextsText_Text t=TextsText.Check();
+	}	
+}
